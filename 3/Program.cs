a31f574241efea7138cd465dde5e06ce641f6a3a
Kyle Ballagher <k.ballagher@gmail.com>
2022-12-17 15:54:07 +0000

    Day 3 Pt 1

---
+++ 3/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code: Day 3");
+
+StreamReader reader = File.OpenText("input.txt");
+var fullText = reader.ReadToEnd();
+
+var rucksacks = fullText.Split("\r\n").ToList();
+
+var duplicateItems = new List<char>();
+
+foreach (var rucksack in rucksacks)
+{
+    var compartment1 = rucksack.Substring(0, rucksack.Length / 2);
+    var compartment2 = rucksack.Substring(rucksack.Length/2);
+
+    foreach (var character in compartment1)
+    {
+        if (compartment2.Contains(character))
+        {
+            duplicateItems.Add(character);
+            break;
+        }        
+    }
+}
+
+var totalValue = 0;
+
+foreach (var item in duplicateItems)
+{
+    var value = (int)item % 32;
+    if (char.IsUpper(item))
+        value = value + 26;
+    
+    totalValue += value;
+}
+
+Console.WriteLine("Total Value: " + totalValue);
+
+//Pt 2
+
+var rucksackGroups = rucksacks.Chunk(3);
+
+Console.WriteLine("Done!");
